0d7b4db47fbc13c8e43c2ef341f6f3bfd4681a52
Kelly Camacho <yired007@gmail.com>
2026-07-17 19:01:41 -0500

    Se eliminan comentarios de ProductoRepository
    Removed commented line for clarity.

--- src/main/java/com/sistemaventas/ventas_api/repository/ProductoRepository.java
+++ src/main/java/com/sistemaventas/ventas_api/repository/ProductoRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    //Buscar producto por nombre
     Optional<Producto> findByNombre(String nombre);
 }
